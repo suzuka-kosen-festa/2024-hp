251f1402ff9b16307fb662a0450fa5f56696fa30
Nao Kitamura <77913295+nao0228@users.noreply.github.com>
2024-05-23 11:55:55 +0900

    feat: add color to design token (#65)

--- app/styles/theme.css.ts
+++ app/styles/theme.css.ts
@@ -1,5 +1,28 @@
-import { createGlobalTheme, createGlobalThemeContract } from "@vanilla-extract/css";
+import { createGlobalTheme, createThemeContract } from "@vanilla-extract/css";
 
-export const vars = createGlobalThemeContract({});
+export const vars = createThemeContract({
+	color: {
+		/**
+		 * #C81A54
+		 */
+		red: null,
+		/**
+		 * #6B007C
+		 */
+		purple: null,
+		/**
+		 * #3227A7
+		 */
+		blue: null,
 
-createGlobalTheme(":root", vars, {});
+	},
+
+});
+
+createGlobalTheme(":root", vars, {
+	color: {
+		red: "#C81A54",
+		purple: "#6B007C",
+		blue: "#3227A7",
+	},
+});
