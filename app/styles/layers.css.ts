944c3bde95940ae8a60449f891e65c6bb8281b72
Rintaro Itokawa <me@re-taro.dev>
2024-06-18 11:05:54 +0900

    refactor: remove componentBase and feature.base is moved to component

--- app/styles/layers.css.ts
+++ app/styles/layers.css.ts
@@ -1,7 +1,7 @@
 import { globalLayer } from "@vanilla-extract/css";
 
 export const reset = globalLayer("reset");
-export const component = globalLayer("component");
-export const componentBase = globalLayer({ parent: component }, "base");
+export const feature = globalLayer("feature");
+export const component = globalLayer({ parent: feature }, "base");
 export const layout = globalLayer("layout");
 export const page = globalLayer("page");
