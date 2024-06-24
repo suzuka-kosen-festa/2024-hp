082056001fa0c4885f386d18604b4eaacc4e695d
ka1ut <tka1utjp@gmail.com>
2024-06-24 22:43:13 +0900

    fix HTML content

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import * as styles from "./style.css";
 export function Footer(): ReactNode {
 	return (
 		<footer className={styles.footer}>
-			<h1>鈴鹿工業高等専門学校 高専祭実行委員</h1>
+			<p>鈴鹿工業高等専門学校 高専祭実行委員</p>
 		</footer>
 	);
 }
