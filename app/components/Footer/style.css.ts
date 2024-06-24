eedfd5a2ad05312fe078d9a4ab6f4222eb7c5254
ka1ut <tka1utjp@gmail.com>
2024-06-24 19:50:07 +0900

    fix for lint

--- app/components/Footer/style.css.ts
+++ app/components/Footer/style.css.ts
@@ -6,25 +6,25 @@ import { Breakpoints } from "@/styles/media.css";
 export const footer = style({
 	"@layer": {
 		[layers.component]: {
-			backgroundColor: vars.color.gray,
-            bottom: 0,
-            width: "100%",
-            position: "fixed",
+			"backgroundColor": vars.color.gray,
+			"bottom": 0,
+			"width": "100%",
 
-            color: vars.color.white,
-            textAlign: "center",
-            fontSize: "0.7rem",
-            
-            paddingBottom: "1.05rem",
-            paddingTop: "2.45rem",
+			"color": vars.color.white,
+			"textAlign": "center",
+			"fontSize": "0.7rem",
+			"letterSpacing": "0.125rem",
 
-            "@media":{
-                [Breakpoints.md]: {
-                    fontSize: "1rem",
-                    paddingBottom: "1.5rem",
-                    paddingTop: "3.5rem",
-                }
-            }
+			"paddingBottom": "1.05rem",
+			"paddingTop": "2.45rem",
+
+			"@media": {
+				[Breakpoints.md]: {
+					fontSize: "1rem",
+					paddingBottom: "1.5rem",
+					paddingTop: "3.5rem",
+				},
+			},
 		},
 	},
 });
