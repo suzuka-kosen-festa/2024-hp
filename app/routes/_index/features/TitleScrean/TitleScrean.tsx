762a2c7b5e45a394220985a40301a99c3125c3ef
Nao Kitamura <kitamurabetuaka@gmail.com>
2024-06-25 00:07:36 +0900

    blank change

--- app/routes/_index/features/TitleScrean/TitleScrean.tsx
+++ app/routes/_index/features/TitleScrean/TitleScrean.tsx
@@ -1,6 +1,6 @@
-import { Countdown } from "@/components/Countdown";
 import type { ReactNode } from "react";
 import * as styles from "./styles.css";
+import { Countdown } from "@/components/Countdown";
 
 export function TitleScrean(): ReactNode {
 	return (
