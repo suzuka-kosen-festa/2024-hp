926d25a0f6c7cb42433089acaf1555b122ade6a9
Rintaro Itokawa <me@re-taro.dev>
2024-06-30 01:37:10 +0900

    feat: add top splash

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,23 +1,29 @@
 import { HeroSection } from "./features/HeroSection";
 import { OverviewSection } from "./features/OverviewSection";
 import { PhilosophySection } from "./features/PhilosophySection";
+import { TitleScrean } from "./features/TitleScrean";
 import * as styles from "./styles.css";
 
 export default function Page() {
 	return (
-		<div>
-			<picture>
-				<source media="(min-height: 2160px)" srcSet="/images/background@7x.webp" type="image/webp" />
-				<source media="(min-height: 1800px)" srcSet="/images/background@6x.webp" type="image/webp" />
-				<source media="(min-height: 1440px)" srcSet="/images/background@5x.webp" type="image/webp" />
-				<source media="(min-height: 1152px)" srcSet="/images/background@4x.webp" type="image/webp" />
-				<source media="(min-height: 1080px)" srcSet="/images/background@3x.webp" type="image/webp" />
-				<source media="(min-height: 768px)" srcSet="/images/background@2x.webp" type="image/webp" />
-				<img className={styles.background} src="/images/background.webp" alt="" />
-			</picture>
-			<HeroSection />
-			<PhilosophySection />
-			<OverviewSection />
-		</div>
+		<>
+			<div className={styles.splashWrapper} id="splash">
+				<TitleScrean />
+			</div>
+			<div id="top">
+				<picture>
+					<source media="(min-height: 2160px)" srcSet="/images/background@7x.webp" type="image/webp" />
+					<source media="(min-height: 1800px)" srcSet="/images/background@6x.webp" type="image/webp" />
+					<source media="(min-height: 1440px)" srcSet="/images/background@5x.webp" type="image/webp" />
+					<source media="(min-height: 1152px)" srcSet="/images/background@4x.webp" type="image/webp" />
+					<source media="(min-height: 1080px)" srcSet="/images/background@3x.webp" type="image/webp" />
+					<source media="(min-height: 768px)" srcSet="/images/background@2x.webp" type="image/webp" />
+					<img className={styles.background} src="/images/background.webp" alt="" />
+				</picture>
+				<HeroSection />
+				<PhilosophySection />
+				<OverviewSection />
+			</div>
+		</>
 	);
 }
