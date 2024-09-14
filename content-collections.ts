2f39bcb9e78db6aa8ffe70161d6b59b30bb8bbda
Rintaro Itokawa <me@re-taro.dev>
2024-09-15 03:00:21 +0900

    feat: add stage_event content data

--- content-collections.ts
+++ content-collections.ts
@@ -48,7 +48,12 @@ const liveEvent = defineCollection({
 		}, { message: "Value of \"endAt\" must be a valid Temporal.ZonedDateTime." }),
 		name: z.string().max(20, { message: "Value of \"name\" must be less than 20 characters." }),
 		overview: z.string().max(100, { message: "Value of \"overview\" must be less than 100 characters." }),
-		press: z.string().url({ message: "Value of \"press\" must be a valid URL." }),
+		press: z.string().refine((v) => {
+			if (/^\/images\/.*/.test(v))
+				return true;
+			else
+				return false;
+		}, { message: "Value of \"press\" must be a valid image URL." }),
 		stage: z.union([z.literal("main"), z.literal("sub")]),
 		startAt: z.string().refine((v) => {
 			try {
---
+++ contents/stage_event/vocal1.md
@@ -0,0 +1,6 @@
+---
+endAt: 2024-11-02T14:45:00[Asia/Tokyo]
+overview: 響かせろ、秘めた想い、歌にのせて。
+startAt: 2024-11-02T13:45:00[Asia/Tokyo]
+title: VOCAL1 GP
+---
