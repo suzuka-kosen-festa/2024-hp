5a836b558c94ee8db5dc821dc68b4d6e94cdf07d
Rintaro Itokawa <me@re-taro.dev>
2024-09-17 16:28:07 +0900

    fix: fix sorting contents

--- app/routes/events/handlers/loader.ts
+++ app/routes/events/handlers/loader.ts
@@ -3,9 +3,9 @@ import { Temporal } from "temporal-polyfill";
 
 export async function loader() {
 	const { allGameEvents, allLiveEvents, allStageEvents } = await import("content-collections");
-	const gameEvents = allGameEvents.sort((a, b) => Temporal.ZonedDateTime.compare(b.date, a.date));
-	const liveEvents = allLiveEvents.sort((a, b) => Temporal.ZonedDateTime.compare(b.startAt, a.startAt));
-	const stageEvents = allStageEvents.sort((a, b) => Temporal.ZonedDateTime.compare(b.startAt, a.startAt));
+	const gameEvents = allGameEvents.sort((a, b) => Temporal.ZonedDateTime.compare(a.date, b.date));
+	const liveEvents = allLiveEvents.sort((a, b) => Temporal.ZonedDateTime.compare(a.startAt, b.startAt));
+	const stageEvents = allStageEvents.sort((a, b) => Temporal.ZonedDateTime.compare(a.startAt, b.startAt));
 
 	return json({
 		gameEvents,
