72f62069ff12f16e020724a711cb7e455cb817fd
ka1ut <tka1utjp@gmail.com>
2024-10-24 01:51:36 +0900

    fixed

--- app/routes/top/features/PhilosophySection/PhilosophySection.tsx
+++ app/routes/top/features/PhilosophySection/PhilosophySection.tsx
@@ -4,31 +4,39 @@ import { SectionTitle } from "@/components/SectionTitle";
 
 export function PhilosophySection(): ReactNode {
 	return (
-		<section aria-label="philosophy section" className={styles.section} id="philosophy">
+		<section
+			aria-label="philosophy section"
+			className={styles.section}
+			id="philosophy"
+		>
 			<SectionTitle icon="message" variant="red" />
 			<div className={styles.box}>
-				<span
-					className={styles.lattice}
-					role="presentation"
-				/>
-				<span
-					className={styles.vertical}
-					role="presentation"
-				/>
-				<h2 className={styles.border}>BORDER</h2>
-				<p className={styles.text}>
-					今年の高専祭のテーマは
-					<wbr />
-					「BORDER」です。
-					<br />
-					高専祭に関わるすべての人が
-					<wbr />
-					”境界”を越えて協力し、
-					<br />
-					高専祭を日常と非日常の“境界”にする。
-					<wbr />
-					という想いが込められています。
-				</p>
+				<div className={styles.background}>
+					<span className={styles.lattice} role="presentation" />
+				</div>
+
+				<div className={styles.content}>
+					<div className={styles.headingWrapper}>
+						<h2 className={styles.border}>BORDER</h2>
+						<span className={styles.vertical} role="presentation" />
+					</div>
+
+					<div className={styles.textContent}>
+						<p className={styles.text}>
+							今年の高専祭のテーマは
+							<wbr />
+							「BORDER」です。
+							<br />
+							高専祭に関わるすべての人が
+							<wbr />
+							”境界”を越えて協力し、
+							<br />
+							高専祭を日常と非日常の“境界”にする。
+							<wbr />
+							という想いが込められています。
+						</p>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
