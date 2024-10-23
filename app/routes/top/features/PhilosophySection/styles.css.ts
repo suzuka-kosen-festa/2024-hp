604d21ee14122c5d30288343c9f051f60f81d188
Kai Tanaka <108340480+ka1ut@users.noreply.github.com>
2024-10-24 02:28:51 +0900

    fix: rewrite philosophy alignment layout (#494)
    Co-authored-by: Bayathy <misosoup0321@icloud.com>

--- app/routes/top/features/PhilosophySection/styles.css.ts
+++ app/routes/top/features/PhilosophySection/styles.css.ts
@@ -8,20 +8,32 @@ export const section = style({
 		[layers.feature]: {
 			"@media": {
 				[Breakpoints.md]: {
-					paddingInline: "4rem",
-					rowGap: "2.875rem",
+					padding: "6rem 4rem 0",
 				},
 			},
-			"backgroundColor": "rgb(0 0 0 / 0.8)",
-			"display": "flex",
-			"flexDirection": "column",
-			"height": "100%",
-			"minHeight": "100svh",
+			"backgroundColor": "rgba(0, 0, 0, 0.8)",
+			"display": "grid",
+			"gridTemplateRows": "auto 1fr",
+			"minHeight": "100vh",
 			"paddingInline": "1rem",
 			"paddingTop": "6rem",
-			"rowGap": "4.75rem",
-
+			"placeItems": "center",
+			"position": "relative",
 			"width": "100%",
+		},
+	},
+});
+
+export const background = style({
+	"@layer": {
+		[layers.feature]: {
+			height: "100%",
+			left: "50%",
+			overflow: "hidden",
+			position: "absolute",
+			top: 0,
+			transform: "translateX(-50%)",
+			width: "100%",
 		},
 	},
 });
@@ -36,8 +48,28 @@ export const box = style({
 			},
 			"height": "20rem",
 			"position": "relative",
-
 			"width": "100%",
+		},
+	},
+});
+
+export const content = style({
+	"@layer": {
+		[layers.feature]: {
+			"@media": {
+				[Breakpoints.md]: {
+					padding: "4rem",
+				},
+			},
+			"display": "flex",
+			"flexDirection": "column",
+			"height": "100%",
+			"justifyContent": "center",
+			"marginInline": "auto",
+			"position": "absolute",
+			"textAlign": "center",
+			"width": "100%",
+			"zIndex": 1,
 		},
 	},
 });
@@ -51,59 +83,65 @@ export const lattice = style({
 				},
 			},
 			"backgroundImage": `linear-gradient(0deg, transparent 0, transparent calc(100% - 1px), ${vars.color.borderRed} calc(100% - 1px), ${vars.color.borderRed} 100%),
-				linear-gradient(90deg, transparent 0, transparent calc(100% - 1px), ${vars.color.borderRed} calc(100% - 1px), ${vars.color.borderRed} 100%)`,
-			"backgroundPosition": "center center",
+        linear-gradient(90deg, transparent 0, transparent calc(100% - 1px), ${vars.color.borderRed} calc(100% - 1px), ${vars.color.borderRed} 100%)`,
+			"backgroundPosition": "center",
 			"backgroundRepeat": "repeat",
 			"backgroundSize": "1.875rem 2.375rem",
 			"height": "100%",
-			"left": "50%",
-			"maxWidth": "72.5rem",
+			"left": 0,
 			"position": "absolute",
 			"top": 0,
-			"transform": "translateX(-50%)",
-
 			"width": "100%",
 		},
 	},
 });
 
+export const headingWrapper = style({
+	alignItems: "center",
+	display: "flex",
+	flexDirection: "column",
+	justifyContent: "center",
+	padding: "2rem 0",
+	position: "relative",
+	width: "100%",
+});
+
 export const vertical = style({
-	"@layer": {
-		[layers.feature]: {
-			backgroundImage: `linear-gradient(to left, ${vars.color.black} 0%, ${vars.color.red} 50%, ${vars.color.black} 100%)`,
-			height: "0.25rem",
-			left: 0,
-			position: "absolute",
-			top: "50%",
-			transform: "translateY(-50%)",
-			width: "100%",
-		},
-	},
+	backgroundImage: `linear-gradient(to left, ${vars.color.black} 0%, ${vars.color.red} 50%, ${vars.color.black} 100%)`,
+	height: "0.25rem",
+	left: 0,
+	position: "absolute",
+	top: "50%",
+	transform: "translateY(-50%)",
+	width: "100%",
 });
 
 export const border = style({
+	"@media": {
+		[Breakpoints.md]: {
+			fontSize: "7.5rem",
+			letterSpacing: "1.5rem",
+			textShadow: `1rem 1rem 1px ${vars.color.red}`,
+		},
+	},
+	"color": vars.color.white,
+	"fontFamily": vars.typography.fontFamily.jura,
+	"fontSize": "3rem",
+	"fontWeight": 300,
+	"letterSpacing": "0.6rem",
+	"textAlign": "center",
+	"textShadow": `0.5rem 0.5rem 1px ${vars.color.red}`,
+	"zIndex": 1,
+});
+
+export const textContent = style({
 	"@layer": {
 		[layers.feature]: {
-			"@media": {
-				[Breakpoints.md]: {
-					fontSize: "7.5rem",
-					letterSpacing: "1.5rem",
-					textShadow: `1rem 1rem 1px ${vars.color.red}`,
-				},
-			},
-			"color": vars.color.white,
-			"fontFamily": vars.typography.fontFamily.jura,
-			"fontSize": "3rem",
-			"fontStyle": "normal",
-			"fontWeight": 300,
-			"left": "50%",
-			"letterSpacing": "0.6rem",
-			"position": "absolute",
-			"textAlign": "center",
-			"textShadow": `0.5rem 0.5rem 1px ${vars.color.red}`,
-			"top": "48%",
-
-			"transform": "translate(-48%, -50%)",
+			alignItems: "center",
+			display: "flex",
+			flexDirection: "column",
+			justifyContent: "center",
+			width: "100%",
 		},
 	},
 });
@@ -115,21 +153,13 @@ export const text = style({
 				[Breakpoints.md]: {
 					fontSize: "1.5rem",
 					letterSpacing: "0.3rem",
-					top: "70%",
-					transform: "translateY(-70%)",
 				},
 			},
 			"color": vars.color.white,
 			"fontSize": "1rem",
-			"hangingPunctuation": "allow-end",
 			"letterSpacing": "0.2rem",
-			"lineBreak": "strict",
-			"overflowWrap": "anywhere",
-			"position": "relative",
-			"textAlign": "center",
-			"top": "80%",
-
-			"wordBreak": "keep-all",
+			"lineHeight": "1.5",
+			"maxWidth": "60rem",
 		},
 	},
 });
