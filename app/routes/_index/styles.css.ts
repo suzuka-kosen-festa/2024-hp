a20495a07e0bcc99e267d1fcfab8b14bfad2186a
Rintaro Itokawa <me@re-taro.dev>
2024-10-24 11:38:43 +0900

    feat: add prologue text

--- app/routes/_index/styles.css.ts
+++ app/routes/_index/styles.css.ts
@@ -1,6 +1,7 @@
 import { style } from "@vanilla-extract/css";
 import * as layers from "@/styles/layers.css";
 import { vars } from "@/styles/theme.css";
+import { Breakpoints } from "@/styles/media.css";
 
 export const landingWrapper = style({
 	"@layer": {
@@ -12,6 +13,35 @@ export const landingWrapper = style({
 			justifyContent: "center",
 			minHeight: "100svh",
 			width: "100%",
+		},
+	},
+});
+
+export const prologueWrapper = style({
+	"@layer": {
+		[layers.page]: {
+			alignItems: "center",
+			display: "none",
+			height: "100%",
+			justifyContent: "center",
+			minHeight: "100svh",
+			width: "100%",
+		},
+	},
+});
+
+export const prologue = style({
+	"@layer": {
+		[layers.page]: {
+			"@media": {
+				[Breakpoints.md]: {
+					fontSize: "1.5rem",
+				},
+			},
+			"color": vars.color.white,
+			"fontFamily": vars.typography.fontFamily.dot,
+			"fontSize": "1rem",
+			"whiteSpace": "pre-line",
 		},
 	},
 });
